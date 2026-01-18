6f9a7f808143a6377aa23df32b6011be812c8857
Cézar Augusto Meira Carmo <dataengineercezar@gmail.com>
2026-01-18 12:51:07 -0300

    fix: Comment module outputs in Terraform skeleton

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
-output "s3_bucket_name" {
-  description = "S3 bucket name"
-  value       = module.s3.bucket_name
-}
+# Outputs - will be uncommented as modules are created
+
+# output "s3_bucket_name" {
+#   description = "S3 bucket name"
+#   value       = module.s3.bucket_name
+# }
 
 output "s3_raw_prefix" {
   description = "S3 RAW prefix"
@@ -13,22 +15,22 @@ output "s3_refined_prefix" {
   value       = "refined/dataset=${var.dataset_name}/ticker=${var.ticker}/"
 }
 
-output "glue_job_name" {
-  description = "Glue Job name"
-  value       = module.glue.job_name
-}
+# output "glue_job_name" {
+#   description = "Glue Job name"
+#   value       = module.glue.job_name
+# }
 
-output "glue_database_name" {
-  description = "Glue Database name"
-  value       = module.glue.database_name
-}
+# output "glue_database_name" {
+#   description = "Glue Database name"
+#   value       = module.glue.database_name
+# }
 
-output "lambda_function_name" {
-  description = "Lambda function name"
-  value       = module.lambda.function_name
-}
+# output "lambda_function_name" {
+#   description = "Lambda function name"
+#   value       = module.lambda.function_name
+# }
 
-output "athena_workgroup_name" {
-  description = "Athena workgroup name"
-  value       = module.athena.workgroup_name
-}
+# output "athena_workgroup_name" {
+#   description = "Athena workgroup name"
+#   value       = module.athena.workgroup_name
+# }
